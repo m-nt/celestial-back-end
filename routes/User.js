e2dd508e5fa29aaa997bf6694936b6a92d3b1309
mohamad nosratian <68176509+mohamadnosratian@users.noreply.github.com>
2022-04-30 17:52:27 +0430

    update

--- routes/User.js
+++ routes/User.js
@@ -7,6 +7,49 @@ const passport = require("passport");
 const User = require("../models/User");
 const router = express.Router();
 
+router.post("/register", upload.none(), (req, res) => {
+  const { Username, Password, CPassword } = req.body;
+  if (!Username || !Password || !CPassword) {
+    return res.send({ message: "please fill in all the required fields !", code: "nok" });
+  }
+  if (Password != CPassword) {
+    return res.send({ message: "Passwords doesn't match !", code: "nok" });
+  }
+  if (Password.length < 6) {
+    return res.send({ message: "Password must be greater than 6 character !", code: "nok" });
+  }
+
+  User.findOne({ Username: Username })
+    .then((user) => {
+      if (user) {
+        res.send({ message: "This Username is taken, try another one!", code: "nok" });
+      } else {
+        const NewUser = new User({
+          Username,
+          Password,
+        });
+        bcrypt
+          .genSalt(15)
+          .then((salt) => {
+            bcrypt
+              .hash(NewUser.Password, salt)
+              .then((hash) => {
+                NewUser.Password = hash;
+                NewUser.save()
+                  .then((_user) => {
+                    res.send({ message: "register successfully done", code: "ok", user: _user });
+                  })
+                  .catch((err) => console.log(err));
+              })
+              .catch((err) => console.log(err));
+          })
+          .catch((err) => console.log(err));
+      }
+    })
+    .catch((err) => {
+      res.send({ message: err.message, code: "nok" });
+    });
+});
 router.post("/loggin", upload.none(), passport.authenticate("local"), (req, res) => {
   User.findOneAndUpdate({ _id: req.user._id }, { token: "connect.sid=" + req.cookies["connect.sid"] })
     .then((user) => {
