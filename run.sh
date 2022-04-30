26d61150ddbdb48d669766a4105001581d25d4a0
mohamad nosratian <68176509+mohamadnosratian@users.noreply.github.com>
2022-04-30 18:14:41 +0430

    update

--- run.sh
+++ run.sh
@@ -5,5 +5,5 @@ docker images
 read IMAGE
 docker image rm $IMAGE
 docker build . -t server
-docker run -d -p 127.0.0.1:8080:8080 --link mongodb:mongodb --name main server
+docker run -d -p 127.0.0.1:8080:8080 --restart always --link mongodb:mongodb --name main server
 docker logs -f main
