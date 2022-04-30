8e4daa09f924eef50c8453500173a14ecc3a24d3
mohamad nosratian <68176509+mohamadnosratian@users.noreply.github.com>
2022-04-30 16:22:49 +0430

    update

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+git pull
+docker rm -f auth
+docker images
+read IMAGE
+docker image rm $IMAGE
+docker build . -t server
+docker run -d -p 127.0.0.1:8080:8080 --link mongodb:mongodb --name main server
+docker logs -f main
